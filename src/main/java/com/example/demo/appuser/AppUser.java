153f3486c429f27922e9d171d21b66d3d367ad7b
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-06-20 11:12:08 -0500

    crear clase AppUser implementando security con UserDetails

---
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -0,0 +1,51 @@
+package com.example.demo.appuser;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AppUser implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
