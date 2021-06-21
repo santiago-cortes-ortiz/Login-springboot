951cf2a79b0e4133c87d06d57fad04ad9bb870e4
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-06-21 17:09:35 -0500

    crear el repository y metodo buscar por email al usuario

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -3,7 +3,9 @@ package com.example.demo.appuser;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppUserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
