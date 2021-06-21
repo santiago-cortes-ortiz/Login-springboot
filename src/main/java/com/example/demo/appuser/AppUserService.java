b67a07ecbcfb7808c094d31e7b5274a1d83a30d6
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-06-21 17:01:07 -0500

    crear la clase UserService e imp'lementar UserDetailServices

---
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.appuser;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
