1b70c55b5f276dfd0504ba3cfd59c39528a3fb11
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-06-21 17:21:56 -0500

    completar userService y manejo de excepcion

--- src/main/java/com/example/demo/appuser/AppUserRepository.java
+++ src/main/java/com/example/demo/appuser/AppUserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface StudentRepository {
+public interface AppUserRepository {
     Optional<AppUser> findByEmail(String email);
 }
